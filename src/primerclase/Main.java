d560432182a361ca96bab67efa0e9ca4654a3c2e
Estudiante <Estudiante@10.203.1.205>
2018-02-15 08:40:34 -0500

    Version 1.0 Hola_mundo

---
+++ src/primerclase/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerclase;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Main {
+    public static void main(String[] args) {
+        //Crear objeto
+        cancion c1 = new cancion();
+        c1.nombre="Close";
+        c1.sonar();
+    }
+}
+
---
+++ src/primerclase/cancion.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerclase;
+
+/**
+ *
+ * @author Daniel
+ */
+public class cancion {
+    //Atributos
+    public String nombre;
+    //Metodos
+    public void sonar(){
+    System.out.println("Sonado" + this.nombre);
+    //Referencia al valor almacenado
+    }
+}
